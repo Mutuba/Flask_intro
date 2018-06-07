51c3c0b9ff3c9be216ed96c5e2914ebcc249c5cb
Mutuba <dmutuba@gmail.com>
2018-06-07 10:23:03 +0300

    First commit for travis build

---
+++ config.py
@@ -0,0 +1,26 @@
+# default config
+import os
+
+
+class BaseConfig(object):
+    DEBUG = False
+    SECRET_KEY = 'cbbfvfjjfjjjfsjajjkkvsjh4636787531r9898298981'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+    # Works for local application but not for deploying to Heroku
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']  # Works for Heroku.
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:baraka@localhost/posts_dev'
+
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
